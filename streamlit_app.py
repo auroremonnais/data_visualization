fd8d284f70123f6b53f8fafc98e8a57fe586fc37
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-10 22:28:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import streamlit as st
 # Page configuration
 st.set_page_config(
     page_title="Olympic Games Medals",
-    page_icon="🥇🥈🥉",
+    page_icon="🥇",
     layout="wide",
     initial_sidebar_state="expanded")
 
 # Sidebar
 with st.sidebar:
-    st.title('🥇🥈🥉 Olympic Games Medals')
+    st.title('🥇 Olympic Games Medals')
 
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
