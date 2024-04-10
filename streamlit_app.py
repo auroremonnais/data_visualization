09eec008ad70c5c449c31397a2c44ba7778b1ede
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-10 22:47:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,7 @@ with st.sidebar:
     year_list = list(olympics.Year.unique())[::-1]
     
     selected_year = st.selectbox('Select a year', year_list)
-    df_selected_year = olympics[olympics.Year == selected_year]
-    df_selected_year_sorted = df_selected_year.sort_values(ascending=True)
+    df_selected_year = olympics[olympics.Year == selected_year].sort_values()
 
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
