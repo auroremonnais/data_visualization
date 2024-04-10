7feb2687c56aab014aec64f7e366a040ab056c86
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-10 22:36:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
+# Imports
 import streamlit as st
+import pandas as pd
+import panel as pn
+import altair as alt
+
+# Panel Extension
+pn.extension()
+pn.extension('vega')
 
 # Page configuration
 st.set_page_config(
@@ -7,9 +15,30 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
+# Data Files Loading
+summer_olympics = pd.read_csv('SummerOlympics.csv')
+winter_olympics = pd.read_csv('WinterOlympics.csv')
+
+# Creation of the 'Season' Column
+summer_olympics['Season']='summer'
+winter_olympics['Season']='winter'
+
+# Concatenation of the two tables to have a unique table
+olympics = pd.concat([summer_olympics, winter_olympics], ignore_index=True).sort_values(by='Year')
+
+# Creation of the columns 'Gold', 'Silver' and 'Bronze'
+olympics['Gold']=olympics['Medal'].apply(lambda x: 1 if x == 'Gold' else 0)
+olympics['Silver']=olympics['Medal'].apply(lambda x: 1 if x == 'Silver' else 0)
+olympics['Bronze']=olympics['Medal'].apply(lambda x: 1 if x == 'Bronze' else 0)
+
 # Sidebar
 with st.sidebar:
     st.title('🥇 Olympic Games Medals')
+
+    # Define dropdown selectors for season, year, and country
+    season_dropdown = pn.widgets.Select(options=['summer', 'winter'], name='Select Season')
+    year_dropdown = pn.widgets.Select(name='Select Year')
+    country_dropdown = pn.widgets.Select(name='Select Country')
 
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
