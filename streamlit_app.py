15b205119a1f1a47d355485e9be6f8eb3762b487
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-10 22:46:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ with st.sidebar:
     year_list = list(olympics.Year.unique())[::-1]
     
     selected_year = st.selectbox('Select a year', year_list)
-    df_selected_year = df_reshaped[df_reshaped.year == selected_year]
+    df_selected_year = olympics[olympics.Year == selected_year]
     df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
 
 x = st.slider("Select a value")
