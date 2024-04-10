5d4f8bf82ea7f339ec317c730761ce30a59d75db
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-10 22:24:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+
+# Page configuration
+st.set_page_config(
+    page_title="US Population Dashboard",
+    page_icon="🏂",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
