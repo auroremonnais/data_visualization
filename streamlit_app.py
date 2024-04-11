5f9921930b32f3b50ab39278b2a13a47915bea11
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-11 08:42:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ st.title('🥇 Olympic Games Medals Data Visualization')
 # Display charts in two columns
 col1, col2 = st.columns(2)
 with col1:
-    st.subheader('Distribution of Medals per Type of Medals')
+    st.subheader('Distribution of Medals per Type')
     st.altair_chart(medal_chart, use_container_width=True)
 with col2:
     st.subheader('Distribution of Medals per Sport')
