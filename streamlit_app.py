88628d650541d2fc65d8ec30082b13dec9d346d3
auroremonnais <115981517+auroremonnais@users.noreply.github.com>
2024-04-11 10:35:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def get_country_data(country):
     return filtered_data
 
 # Define the function to create the visualization based on filtered data
-def create_medal_chart(filtered_data, selection):
+def create_medal_chart(filtered_data):
     # Aggregate the filtered data to calculate total medals per type in each Olympic year
     aggregated_data = filtered_data.groupby(['Year', 'Country', 'Season']).agg(
         Gold=('Gold', 'sum'),
@@ -65,7 +65,7 @@ def create_medal_chart(filtered_data, selection):
     chart = alt.Chart(melted_data).mark_bar().encode(
         x='Medal:N',  # Medal types as x-axis
         y='Count:Q',  # Count of medals as y-axis
-        color=alt.condition(selection, alt.Color('Medal:N', scale=alt.Scale(domain=['Gold', 'Silver', 'Bronze'], range=[medal_colors['Gold'], medal_colors['Silver'], medal_colors['Bronze']])), alt.value('lightgray'))
+        color=alt.Color('Medal:N', scale=alt.Scale(domain=['Gold', 'Silver', 'Bronze'], range=[medal_colors['Gold'], medal_colors['Silver'], medal_colors['Bronze']])),
     ).properties(
         width=400,
         height=400
@@ -79,12 +79,12 @@ def create_sport_chart(filtered_data, selection):
     # Create a pie chart
     chart = alt.Chart(aggregated_data).mark_arc().encode(
         theta='Count:Q',
-        color=alt.condition(selection, alt.Color('Sport:N', legend=None), alt.value('lightgray')),
+        color='Sport:N',
         tooltip=['Country', 'Year', 'Season', 'Sport', 'Count']
     ).properties(
         width=400,
         height=400
-    )
+    ).add_selection(selection)
     return chart
 
 def create_gender_chart(filtered_data):
@@ -132,13 +132,13 @@ def create_gender_chart(filtered_data):
 def update(selection):
     filtered_data = filter_data(year_dropdown, country_dropdown, season_button)
     country_data = get_country_data(country_dropdown)
-    medal_chart = create_medal_chart(filtered_data, selection)
+    medal_chart = create_medal_chart(filtered_data)
     sport_chart = create_sport_chart(filtered_data, selection)
     gender_chart = create_gender_chart(country_data)
     return medal_chart, sport_chart, gender_chart
 
 # Initial selection
-selection = alt.selection_single(empty='all', fields=['Medal'])
+selection = alt.selection_single(empty='all', fields=['Sport'])
 
 # Initial update
 medal_chart, sport_chart, gender_chart = update(selection)
@@ -147,10 +147,10 @@ medal_chart, sport_chart, gender_chart = update(selection)
 col1, col2 = st.columns(2)
 with col1:
     st.subheader('Distribution of Medals per Type')
-    st.altair_chart(medal_chart, use_container_width=True).add_selection(selection)
+    st.altair_chart(medal_chart, use_container_width=True)
 with col2:
     st.subheader('Distribution of Medals per Sport')
-    st.altair_chart(sport_chart, use_container_width=True).add_selection(selection)
+    st.altair_chart(sport_chart, use_container_width=True)
 
 # Display the third chart underneath the existing charts
 st.subheader('Total Number of Medals by Gender Evolution')
